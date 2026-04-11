1aec65b67031be9ef64d5ed2b0e2431eaf8adda2
Maxime HOWILLER <maxime.howiller@edu.devinci.fr>
2026-04-11 09:23:11 +0200

    test 2

--- Entity.h
+++ Entity.h
@@ -7,40 +7,40 @@ using namespace std;
 
 class Entity
 {
-protected:
-    string name;
-    int hpMax;
-    int hp;
-    int at;
-    int de;
+    protected:
+        string name;
+        int hpMax;
+        int hp;
+        int at;
+        int de;
 
-public:
+    public:
 
-    Entity(string pname, int phpmax, int php, int pat, int pde)
-    {
-        name = pname;
-        hpMax = phpmax;
-        hp = php;
-        at = pat;
-        de = pde;
-    }
-    
-    virtual void takeDamage(int amount)
-    {
-        hp -= (amount - de);
-        if (hp < 0)
+        Entity(string pname, int phpmax, int php, int pat, int pde)
         {
-            hp = 0;
-        } 
-    }
+            name = pname;
+            hpMax = phpmax;
+            hp = php;
+            at = pat;
+            de = pde;
+        }
+        
+        virtual void takeDamage(int amount)
+        {
+            hp -= (amount - de);
+            if (hp < 0)
+            {
+                hp = 0;
+            } 
+        }
 
-    bool islandAlive()
-    {
-        return hp > 0;
-    }
+        bool islandAlive()
+        {
+            return hp > 0;
+        }
 
-    string getName()
-    {
-        return name;
-    }
+        string getName()
+        {
+            return name;
+        }
 };
--- GameManager.h
+++ GameManager.h
@@ -8,25 +8,25 @@ using namespace std;
 
 class GameManager
 {
-private:
-    Player* player;
-    Monster* currentMonster;
+    private:
+        Player* player;
+        Monster* currentMonster;
 
-public:
-    GameManager(Player* p, Monster* m)
-    {
-        player = p;
-        currentMonster = m;
-    }
+    public:
+        GameManager(Player* p, Monster* m)
+        {
+            player = p;
+            currentMonster = m;
+        }
 
-    void announceBattle()
-    {
-        cout << "BATAILLE : " << player->getName() << " VS " << currentMonster->getName() << endl;
-    }
+        void announceBattle()
+        {
+            cout << "BATAILLE : " << player->getName() << " VS " << currentMonster->getName() << endl;
+        }
 
-    void processTurn()
-    {
-        currentMonster->takeDamage(15);
-        cout << "Le joueur attaque " << currentMonster->getName() << endl;
-    }
+        void processTurn()
+        {
+            currentMonster->takeDamage(15);
+            cout << "Le joueur attaque " << currentMonster->getName() << endl;
+        }
 };
